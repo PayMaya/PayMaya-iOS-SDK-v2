35159ee33a469da4247f30a53a506301d57736ef
Maciej Górecki <maciej.gorecki@miquido.com>
2020-01-21 10:49:20 +0100

    Initial commit.

---
+++ paymaya-sdk/paymaya_sdk.h
@@ -0,0 +1,19 @@
+//
+//  paymaya_sdk.h
+//  paymaya-sdk
+//
+//  Created by Maciej Górecki on 21/01/2020.
+//  Copyright © 2020 Maciej Górecki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for paymaya_sdk.
+FOUNDATION_EXPORT double paymaya_sdkVersionNumber;
+
+//! Project version string for paymaya_sdk.
+FOUNDATION_EXPORT const unsigned char paymaya_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <paymaya_sdk/PublicHeader.h>
+
+
